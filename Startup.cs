ea9983bd88357d676c8d6031c1f074f6d354dd23
lehuunghia121 <85742202+lehuunghia121@users.noreply.github.com>
2021-07-09 19:59:37 +0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab_4_5_7.Startup))]
+namespace Lab_4_5_7
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
